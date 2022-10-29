c37f5548596fc18ec1ed5b9fb33a7f1d0849e825
Ifeanyi Nnamdi-Okagbue <ifeanyi.okagbue@gmail.com>
2022-10-29 08:07:08 +0100

    Add project files.

---
+++ MultiThreadingExample/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hello, Browser from the main thread {Thread.CurrentThread.ManagedThreadId}");
+RunMultipleThreads();
+
+static void RunSingleThread()
+{
+    RunMeFirst();
+    RunMeSecond();
+}
+
+static void RunMultipleThreads()
+{
+    new Thread(RunMeFirst).Start();
+    new Thread(RunMeSecond).Start();
+}
+
+static void RunMeFirst()
+{
+    Console.WriteLine($"Hello from RunMeFirst and Thread {Thread.CurrentThread.ManagedThreadId}");
+    for(int i = 0; i < 30000; i++)
+    {
+        Console.WriteLine($"Ping from RunMeFirst and Thread {Thread.CurrentThread.ManagedThreadId}");
+    }
+}
+
+static void RunMeSecond()
+{
+    Console.WriteLine($"Hello from RunMeSecond and Thread {Thread.CurrentThread.ManagedThreadId}");
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Ping {i} from RunMeSecond and Thread {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+    }
+}
